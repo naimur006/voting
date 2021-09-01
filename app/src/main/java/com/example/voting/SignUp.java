7f9cb0e2bae60bc7097eb60028188eb0f2cf7726
naimur006 <durjoyopu@gmail.com>
2021-09-01 12:57:37 +0600

    online voting system

--- app/src/main/java/com/example/voting/SignUp.java
+++ app/src/main/java/com/example/voting/SignUp.java
@@ -21,47 +21,27 @@ import android.widget.RadioGroup;
 public class SignUp extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener{
     RadioGroup radioGroup;
     String gender;
-    ImageView imageView;
-    Button btnopen;
+    Button button;
 
-    @Override
+       @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        imageView = findViewById(R.id.image_view);
-        btnopen = findViewById(R.id.btn_open);
+
         radioGroup = (RadioGroup) findViewById(R.id.gender_group);
         radioGroup.setOnCheckedChangeListener(this);
-
-        //Request for Camera permission
-        if (ContextCompat.checkSelfPermission(SignUp.this,
-                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(SignUp.this,
-                    new String[]{
-                            Manifest.permission.CAMERA
-                    },
-                    100);
-        }
-        btnopen.setOnClickListener(new View.OnClickListener() {
+        button = (Button) findViewById(R.id.nextforcameraoption);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //open camera
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,100);
+                Intent intent=new Intent(SignUp.this,SignUpForCamera.class);
+                startActivity(intent);
             }
         });
+
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode == 100){
-            //Get Capture Image
-            Bitmap captureimage = (Bitmap) data.getExtras().get("data");
-            //Set capture image to imageview
-            imageView.setImageBitmap(captureimage);
-        }
-    }
 
     @Override
     public void onCheckedChanged(RadioGroup group, int i) {
---
+++ app/src/main/java/com/example/voting/SignUpForCamera.java
@@ -0,0 +1,14 @@
+package com.example.voting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpForCamera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_for_camera2);
+    }
+}
